01f9a10dbab0b222d8da2f85319a1ef22db8b97c
SatwikMohan <satwikplay69@gmail.com>
2023-12-10 17:02:34 +0530

    asssignment start

---
+++ lib/functions.dart
@@ -0,0 +1,15 @@
+import 'package:permission_handler/permission_handler.dart';
+
+void callForPermissions() async{
+  await Permission.storage;
+  await Permission.audio;
+  await Permission.speech;
+}
+
+int getSeconds (int time){
+  return time%60;
+}
+
+int getMinutes(int time){
+  return (time/60).toInt();
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,137 @@
+import 'dart:io';
+
+import 'package:assignment/functions.dart';
+import 'package:audio_waveforms/audio_waveforms.dart';
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  SpeechToText _speechToText = SpeechToText();
+  RecorderController controller = RecorderController();
+  bool _speechEnabled = false;
+  int time = 0;
+  String _lastWords = "";
+  String text = "Start Speaking ....";
+  String displayText = "";
+  int f= 0;
+
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {
+      text = "Listening ...";
+    });
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {
+      text = "Start Speaking ....";
+    });
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+      if(_lastWords.length<MediaQuery.of(context).size.width/3){
+        displayText = _lastWords;
+      }else{
+        if(f==0){
+          displayText+="...";
+        }
+        f=1;
+      }
+    });
+  }
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initSpeech();
+    controller = RecorderController()
+      ..updateFrequency = const Duration(milliseconds: 100)
+      ..normalizationFactor = Platform.isAndroid ? 60 : 40;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(' Audiofy ',
+          style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Center(
+              child: Text(' ${getMinutes(time)} : ${getSeconds(time)} ',
+                style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 40),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(text,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width/3,
+              height: 20,
+              child: Text(displayText,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 12),),
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height/6,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(onPressed: (){
+              _startListening();
+              controller.record();
+            },
+                icon: Icon(Icons.mic,color: Colors.white,size: 50,),
+            ),
+          ),
+          AudioWaveforms(
+              size: Size(MediaQuery.of(context).size.width, 30.0),
+              recorderController: controller,
+              enableGesture: true,
+              waveStyle: WaveStyle(
+                backgroundColor: Colors.white,
+                showDurationLabel: true,
+                spacing: 8.0,
+                showBottom: false,
+                extendWaveform: true,
+                showMiddleLine: false,
+              ),
+          ),
+          SizedBox(height: 160,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('Press Mic To start listening',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 12),),
+              Text('Swipe Right to stop and Save',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 12),),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
